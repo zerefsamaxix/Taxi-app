1ec21d33e4e14370d678801bfb1b615823245679
David Verdianto <61915681+zerefsamaxix@users.noreply.github.com>
2022-03-25 22:18:28 +0700

    Add files via upload

---
+++ Taxi-app/Taxi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Nama Pengemudi : {0}", DriverName);
+
+            if (OnDuty == true)
+            {
+                Console.WriteLine("sedang bertugas : Ya", OnDuty);
+            }
+            else
+            {
+                Console.WriteLine("sedang bertugas : Tidak", OnDuty);
+            }
+            Console.WriteLine("jumlah penumpang : {0}\n", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai menjemput penumpang\n", DriverName);
+        }
+    }
+}
